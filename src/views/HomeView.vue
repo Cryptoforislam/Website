28ed70fcfbc88a8925d4a11933e2b08c643152f9
Cryptoforislam <110429865+Cryptoforislam@users.noreply.github.com>
2022-08-14 23:38:25 +0100

    Join Telegram

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,23 @@
 <template>
 	<section class="h-auto bg-white overflow-hidden">
-		<div class="max-w-6xl pt-24 lg:pt-32 pb-16 lg:pb-24 xl:px-5 px-12 relative z-20 justify-center mx-auto flex lg:flex-row flex-col items-center sm:text-center lg:text-left">
+		<div
+			class="max-w-6xl pt-24 lg:pt-32 pb-16 lg:pb-24 xl:px-5 px-12 relative z-20 justify-center mx-auto flex lg:flex-row flex-col items-center sm:text-center lg:text-left">
 			<div class="lg:w-1/2 w-full">
 				<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 relative sm:text-5xl">
 					<span class="block xl:inline relative">CRYPTO FOR</span>
-					<span class="block text-6xl sm:text-7xl xl:text-8xl xl:inline-block relative bg-clip-text text-transparent bg-gradient-to-r font-black from-black via-gray-800 to-black">ISLAM</span>
+					<span
+						class="block text-6xl sm:text-7xl xl:text-8xl xl:inline-block relative bg-clip-text text-transparent bg-gradient-to-r font-black from-black via-gray-800 to-black">ISLAM</span>
 				</h1>
-				<p class="my-5 text-lg text-gray-400 mx-auto sm:text-2xl sm:my-9 sm:max-w-3xl md:leading-9 lg:pr-16">Every penny you donate has the power to change a life.</p>
+				<p class="my-5 text-lg text-gray-400 mx-auto sm:text-2xl sm:my-9 sm:max-w-3xl md:leading-9 lg:pr-16">
+					Every penny you donate has the power to change a life.</p>
 				<div class="box-border leading-7 lg:text-left text-gray-900 text-center">
-					<router-link
-						to="/app"
-						class="inline-block w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-gray-900 border border-transparent border-solid rounded-full cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-black hover:text-white"
-					>
+					<router-link to="/app"
+						class="inline-block w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-gray-900 border border-transparent border-solid rounded-full cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-black hover:text-white">
 						DONATE
 					</router-link>
-					<a
-						href="#_"
-						class="inline-block w-full px-5 py-3 text-base font-semibold text-gray-600 no-underline align-middle border border-transparent border-solid rounded-full cursor-pointer select-none bg-gray-100 sm:w-auto hover:bg-gray-200 hover:text-blue-600"
-					>
-						Learn More
-					</a>
+					<a href="https://t.me/cryptoforislam"
+						class="inline-block w-full px-5 py-3 text-base font-semibold text-gray-600 no-underline align-middle border border-transparent border-solid rounded-full cursor-pointer select-none bg-gray-100 sm:w-auto hover:bg-gray-200 hover:text-blue-600">
+						Join Telegram </a>
 				</div>
 			</div>
 			<div class="lg:w-1/2 w-full lg:max-w-none max-w-md lg:pt-0 pt-20 mx-auto lg:pl-20">
@@ -32,7 +30,8 @@
 		<div class="max-w-6xl px-4 px-16 mx-auto leading-6 text-left xl:px-12">
 			<div class="box-border flex flex-col flex-wrap items-start mx-0 text-indigo-900">
 				<div class="pb-4 text-sm font-bold text-left text-gray-700 uppercase">Powerful Features</div>
-				<h3 class="relative w-full px-0 pb-2 m-0 font-sans text-4xl font-bold text-left xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0">
+				<h3
+					class="relative w-full px-0 pb-2 m-0 font-sans text-4xl font-bold text-left xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0">
 					Powerful and Simple Features for your Application
 				</h3>
 			</div>
@@ -42,7 +41,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Water</div>
-							<div class="box-border text-base leading-normal text-gray-700">An ever-flowing charity,Sadaqah jariyah provides clean water so communities can thrive.</div>
+							<div class="box-border text-base leading-normal text-gray-700">An ever-flowing
+								charity,Sadaqah jariyah provides clean water so communities can thrive.</div>
 						</div>
 					</div>
 				</div>
@@ -52,7 +52,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Education</div>
-							<div class="box-border text-base leading-normal text-gray-700">Providing access to education & creating powerful antidotes to unrest, distrust, & war</div>
+							<div class="box-border text-base leading-normal text-gray-700">Providing access to education
+								& creating powerful antidotes to unrest, distrust, & war</div>
 						</div>
 					</div>
 				</div>
@@ -61,7 +62,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Food Security</div>
-							<div class="box-border text-base leading-normal text-gray-700">Delivering urgent food in Ramadan & year round to relieve starvation.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Delivering urgent food in
+								Ramadan & year round to relieve starvation.</div>
 						</div>
 					</div>
 				</div>
@@ -70,7 +72,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Emergency Relief</div>
-							<div class="box-border text-base leading-normal text-gray-700">Helping victims of disaster & violent conflict.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Helping victims of disaster &
+								violent conflict.</div>
 						</div>
 					</div>
 				</div>
@@ -79,7 +82,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Orphan Care</div>
-							<div class="box-border text-base leading-normal text-gray-700">Caring for those who can't care for themselves.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Caring for those who can't
+								care for themselves.</div>
 						</div>
 					</div>
 				</div>
@@ -88,7 +92,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Health & Wellbeing</div>
-							<div class="box-border text-base leading-normal text-gray-700">Leading critical projects to heal bodies, minds, & souls.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Leading critical projects to
+								heal bodies, minds, & souls.</div>
 						</div>
 					</div>
 				</div>
@@ -98,7 +103,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Refugee Empowerment</div>
-							<div class="box-border text-base leading-normal text-gray-700">Alleviating the needs of vulnerable families & children.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Alleviating the needs of
+								vulnerable families & children.</div>
 						</div>
 					</div>
 				</div>
@@ -108,7 +114,8 @@
 						<img src="icon/icon.svg" class="leading-6 text-left align-middle border-none w-14 h-14" />
 						<div class="pl-8 leading-6 text-left">
 							<div class="box-border pb-1 text-xl font-medium text-indigo-900">Livelihoods</div>
-							<div class="box-border text-base leading-normal text-gray-700">Lifting people out of poverty & ensuring their self-reliance.</div>
+							<div class="box-border text-base leading-normal text-gray-700">Lifting people out of poverty
+								& ensuring their self-reliance.</div>
 						</div>
 					</div>
 				</div>
@@ -119,10 +126,14 @@
 	<section class="py-24 bg-white">
 		<div class="px-12 mx-auto max-w-7xl">
 			<div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-				<h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
-					<span class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">100% </span> DONATIONS PROMISE
+				<h1
+					class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+					<span
+						class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">100%
+					</span> DONATIONS PROMISE
 				</h1>
-				<p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">Thanks to the unique way we work we are able to ensure that 100% of your donation goes to our work.</p>
+				<p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">Thanks to the unique way we work we are
+					able to ensure that 100% of your donation goes to our work.</p>
 			</div>
 			<div class="w-full mx-auto mt-20 text-center md:w-10/12 hover:scale-150">
 				<img src="pro.png" />
